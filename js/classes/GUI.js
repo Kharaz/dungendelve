ac6b64d476fd250330f20abb7bf4866325c0858b
PTC <pheartheceal@gmail.com>
2015-01-07 17:43:50 -0800

    some code cleanup

--- js/classes/GUI.js
+++ js/classes/GUI.js
@@ -1,15 +1,15 @@
-var GuiElement = function(sprite,x,y) {
+var GuiElement = function(sprite, x, y) {
     this.sprite = sprite;
     //this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
-}
+};
 
-GuiElement.prototype.render = function(){
+GuiElement.prototype.render = function() {
     //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-GuiElement.prototype.update = function(){
+GuiElement.prototype.update = function() {
 
-}
+};
